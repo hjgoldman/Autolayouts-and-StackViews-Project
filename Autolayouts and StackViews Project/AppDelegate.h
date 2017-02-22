e89032eb285e9e978e4b865754741d66d00484b1
hjgoldman <haydengoldman@gmail.com>
2017-02-22 12:36:07 -0600

    Initial commit

---
+++ Autolayouts and StackViews Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Autolayouts and StackViews Project
+//
+//  Created by Hayden Goldman on 2/22/17.
+//  Copyright © 2017 Hayden Goldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Autolayouts and StackViews Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Autolayouts and StackViews Project
+//
+//  Created by Hayden Goldman on 2/22/17.
+//  Copyright © 2017 Hayden Goldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
